8dc76b01822203d51f8bbbca21219ceac3b3020e
Michael Roark <roarkmichael@gmail.com>
2022-09-02 00:29:32 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='math-898',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Thesis Related Work',
+    author='Michael Roark',
+    license='',
+)
+
