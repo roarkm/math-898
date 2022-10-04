620c596ce5753013a53558c73e3e978036131455
Michael Roark <roarkmichael@gmail.com>
2022-10-03 20:32:57 -0700

    ILP: working?
    Maybe need some threshold on eps < eps_hat comparison?

--- src/algorithms/abstract_verifier.py
+++ src/algorithms/abstract_verifier.py
@@ -19,7 +19,7 @@ class AbstractVerifier():
             return "No Constraints."
         s = ""
         for c in self.constraints:
-            s += f"\nconstraint:"
+            s += f"\nconstraint: "
             s += str(c)
         return s
 
@@ -34,17 +34,17 @@ class AbstractVerifier():
         return s
 
 
-    def sep_hplane_for_advclass(self, x, complement=False):
-        fx = self.f(torch.tensor(np.array(x)).T.float()).detach().numpy()
-        class_order = np.argsort(fx)[0]
-        # index of component with largest value
-        x_class = class_order[-1]
-        # index of component with second largest value
-        adversarial_class = class_order[-2]
-        return _vector_for_separating_hyperplane(large_index=x_class,
-                                                 small_index=adversarial_class,
-                                                 n=fx.shape[1],
-                                                 complement=complement)
+    # def sep_hplane_for_advclass(self, x, complement=False):
+        # fx = self.f(torch.tensor(np.array(x)).T.float()).detach().numpy()
+        # class_order = np.argsort(fx)[0]
+        # # index of component with largest value
+        # x_class = class_order[-1]
+        # # index of component with second largest value
+        # adversarial_class = class_order[-2]
+        # return _vector_for_separating_hyperplane(large_index=x_class,
+                                                 # small_index=adversarial_class,
+                                                 # n=fx.shape[1],
+                                                 # complement=complement)
 
 
 def constraints_for_separating_hyperplane(opt_vars, large_index, small_index,
@@ -54,10 +54,29 @@ def constraints_for_separating_hyperplane(opt_vars, large_index, small_index,
     assert large_index <= opt_vars.shape[1]
     assert small_index <= opt_vars.shape[1]
 
-    c = _vector_for_separating_hyperplane(large_index, small_index, opt_vars.shape[1])
-    if complement:
-        return [c @ opt_vars <= 0]
+    c = _vector_for_separating_hyperplane(large_index, small_index,
+                                          opt_vars.shape[1],
+                                          complement=complement)
     return [c @ opt_vars >= 0]
+
+
+def mat_for_k_class_polytope(k, dim, complement=False):
+    assert complement == False, "Complement option not yet supported"
+    if dim == 1:
+        return np.zeros((1,1)), np.zeros((1,1))
+
+    n_rows = dim-1
+    A = np.zeros((n_rows, dim))
+    A[:,k] = 1
+
+    row = 0
+    for j in range(0, dim):
+        if j == k:
+            continue
+        A[row][j] = -1
+        row += 1
+    b = np.zeros((n_rows, 1))
+    return (A, b)
 
 
 def constraints_for_k_class_polytope(k, x, verbose=False, complement=False):
@@ -66,24 +85,8 @@ def constraints_for_k_class_polytope(k, x, verbose=False, complement=False):
     # ie - the polytope corresponding to the region of R^dim where
     # the k-th component is greater than all other components
     # returns a list containing the inequality constraints in matrix form
-    assert complement == False, "Complement option not yet supported"
     z = cp.bmat([cp.Variable(len(x), name='z')]).T
-    n_rows = len(x)-1
-    A = np.zeros((n_rows, len(x)))
-    A[:,k] = 1
-
-    row = 0
-    for j in range(0, len(x)):
-        if j == k:
-            continue
-        A[row][j] = -1
-        row += 1
-
-    b = np.zeros((n_rows, 1))
-    if verbose:
-        print(f"Polytope for {len(x)}-class classifier. k={k}.")
-        print(A)
-        print(b)
+    A, b = mat_for_k_class_polytope(k, len(x), complement)
     return [A @ z >= b]
 
 
@@ -107,10 +110,11 @@ def weights_from_nn(f):
 
 def _vector_for_separating_hyperplane(large_index, small_index, n, complement=False):
     # create vector c for a seperating hyperplane
-    #    {x | c dot x >= 0 and x,c in R^n}
+    #    {x | c dot x >= 0 and x,c in R^n} = {x | x_large_index > x_small_index }
     c = np.zeros((1, n))
     c[0][large_index] = 1
     c[0][small_index] = -1
+
     if complement:
         return -1 * c.T
     return c.T
--- src/algorithms/ilp.py
+++ src/algorithms/ilp.py
@@ -11,57 +11,25 @@ class IteratedLinearVerifier(AbstractVerifier):
 
     def __init__(self, f=None):
         super(IteratedLinearVerifier, self).__init__(f)
-        self.constraints = list()
+        self.prob = None
 
 
-    def constraints_for_inf_ball(self, center, eps, verbose=False):
-        # create a list containing the constraints for an inf-ball of radius eps
-        # set constraints for hypercube around x
-        A_lt = np.zeros((2, len(center)))
-        a_vec = np.zeros((2, 1))
-
-        # top constraint
-        z0 = cp.bmat([cp.Variable(len(center), name='z0')]).T
-        A_lt[0][0] = 1
-        a_vec[0][0] = center[0][0] + eps
-        # bottom constraint
-        A_lt[1][0] = -1
-        a_vec[1][0] = -1 * (center[0][0] - eps)
-
-        if len(center) > 1:
-            for i in range(1, len(center)):
-                # top constraint
-                row = np.zeros((1, len(center)))
-                row[0][i] = 1
-                A_lt = np.vstack([A_lt, row])
-                _a = np.matrix([[ float(center[i][0]) + eps ]])
-                a_vec = np.vstack([a_vec, _a])
-
-                # bottom constraint
-                row = np.zeros((1, len(center)))
-                row[0][i] = -1
-                A_lt = np.vstack([A_lt, row])
-                _a = np.matrix([[ -1 * float(center[i][0]) - eps ]])
-                a_vec = np.vstack([a_vec, _a])
-
-        if verbose:
-            print(f"Constraints for inf-ball radius {eps} at center {center}")
-            print(A_lt)
-            print(a_vec)
-        return [A_lt @ z0 <= a_vec]
-
-
-    def verify_at_point(self, x=[[9], [0]], eps=0.5):
+    def build_problem_for_point(self, x, verbose=False):
         # use ILP to verify all x' within eps inf norm of x
         # are classified the same as x'
         # (ie: f(x) == f(x') for all x' in inf-norm ball centered at x
         assert self.f != None, "No NN provided."
         assert self.nn_weights[0].shape[1] == len(x), "x is the wrong shape"
 
-        _im_x = np.matrix(x)
+        # _im_x = np.array(x)
+        _im_x = np.reshape(np.array(x), (len(x), 1))
+        # print(f"ref point x: {_im_x.shape}")
+        # print(_im_x)
+
         # z0 is unconstrained since we are using inf-ball in objective function
         # optimization var list starting with z0
         zi_list = [cp.Variable(_im_x.shape, name='z0')]
+        self.opt_var = zi_list[0]
 
         # propogate x layer by layer through f
         # 1) add constraints for affine transforms
@@ -73,21 +41,27 @@ class IteratedLinearVerifier(AbstractVerifier):
 
             # add constraint for affine transformation
             zi_list.append(cp.Variable((Wi.shape[0],1), f"z{i+1}_hat"))
-            # zi_hat = Wi @ zi_list[-2] + bi
+            # print(zi_list[-1] == Wi @ zi_list[-2] + bi)
             self.constraints += [zi_list[-1] == Wi @ zi_list[-2] + bi]
 
-            # add constraints for ReLU activation pattern
-            zi_list.append(cp.Variable((Wi.shape[0],1), f"z{i}"))
             # propogate reference point
-            _im_x = Wi * _im_x + bi
+            _im_x = Wi @ _im_x + bi
+            # print(f"after layer {i} affine T - shape={_im_x.shape}")
+            # print(_im_x)
 
-            # for all but the last layer (last layer has no ReLU)
+            # for all but the last layer
+            # TODO: Do not assume last layer has no ReLU (how to verify?)
             if i < len(self.nn_weights):
-                # build indicator vector to encode inequality constraint on zi_hat as dot product
+                # add constraints for ReLU activation pattern
+                zi_list.append(cp.Variable((Wi.shape[0],1), f"z{i+1}"))
+                # build indicator vector to encode inequality
+                # constraint on zi_hat as dot product
                 delta = np.zeros((1, len(_im_x)))
                 beta  = np.zeros((1, len(_im_x)))
                 for j, _im_x_j in enumerate(_im_x):
-                    if _im_x_j[0,0] >= 0:
+                    # print(f"layer-{i}, _im_x[{j}] = {_im_x_j[0]}")
+                    # continue
+                    if _im_x_j[0] >= 0:
                         # zi_hat[j] >= 0 iff (1 * zi_hat[j] >= 0)
                         delta[0,j] = 1
                         # constraint zi == zi_hat
@@ -105,69 +79,111 @@ class IteratedLinearVerifier(AbstractVerifier):
                 self.constraints += [zi_list[-1] == beta @ zi_list[-2]]
 
                 # continue propogating reference point through f
-                _im_x = np.matrix(self.relu(torch.tensor(_im_x)))
-
+                # print(f"Before relu at layer {i}: shape={_im_x.shape}")
+                # print(_im_x)
+                _im_x = np.array(self.relu(torch.tensor(_im_x)))
+                # print(f"After relu at layer {i}: shape={_im_x.shape}")
+                # print(_im_x)
 
         # Add constraints for safety set
         # Only consider seperating hyperplane for the predicted class of x
         # and the next highest component
-        fx = self.f(torch.tensor(_im_x).T.float()).detach().numpy()
-        class_order = np.argsort(fx)[0]
+        fx = self.f(torch.tensor(x).T.float()).detach().numpy()
+        assert np.array_equal(_im_x, fx.T) == True # sanity check
 
-        x_class = class_order[-1] # index of component with largest value
-        adversarial_class = class_order[-2] # index of component with second largest value
+        _class_order = np.argsort(fx)[0]
+        x_class = _class_order[-1]           # index of component with largest value
+        adversarial_class = _class_order[-2] # index of component with second largest value
+        # print(f"f({x}) = {fx} => Want z_{adversarial_class} > z_{x_class}")
 
-        zn = cp.Variable(fx.shape, name='zn')
-        self.constraints += self.constraints_for_separating_hyperplane(zn, x_class, adversarial_class,
-                                                                      complement=True)
+        self.constraints += constraints_for_separating_hyperplane(zi_list[-1].T, x_class,
+                                                                  adversarial_class,
+                                                                  complement=True)
 
         obj = cp.Minimize(cp.atoms.norm_inf(np.matrix(x) - zi_list[0]))
-        problem = cp.Problem(obj, self.constraints)
-        problem.solve()
+        self.prob = cp.Problem(obj, self.constraints)
+        if verbose:
+            print(self.str_constraints())
 
-        print(f"f({x}) = {fx} |--> class: {x_class}")
-        status = problem.status
+
+    def robustness_at_point(self, x, verbose=False):
+        self.prob.solve(verbose=verbose)
+        status = self.prob.status
         if status == cp.OPTIMAL:
-            if problem.value >= eps:
-                print(f"Opt val: {problem.value} >= {eps}.")
-                return True
-            print(f"Opt val: {problem.value} < {eps}.")
-            return False
-        elif status == cp.INFEASIBLE:
-            # How to check if this is a false negative? (maybe the relaxations aren't tight enough)
-            print(f"COULD NOT verify")
-        elif status == cp.UNBOUNDED:
-            print(f"Problem is unbounded - {cp.OPTIMAL_INACCURATE}")
+            # print(f"f({x}) = {fx} |--> class: {x_class}")
+            return self.prob.value
         elif status == cp.OPTIMAL_INACCURATE:
-            print(f"RUH ROH - {cp.OPTIMAL_INACCURATE}")
-            print(f"SUCCESS?")
-        elif status == cp.INFEASIBLE_INACCURATE:
-            print(f"RUH ROH - {cp.INFEASIBLE_INACCURATE}")
-        elif status == cp.UNBOUNDED_INACCURATE:
-            print(f"RUH ROH - {cp.UNBOUNDED_INACCURATE}")
-        elif status == cp.INFEASIBLE_OR_UNBOUNDED:
-            print(f"RUH ROH - {cp.INFEASIBLE_OR_UNBOUNDED}")
+            print("Warning: inaccurate solution.")
+            return self.prob.value
+        else:
+            raise Exception(status)
 
-        return None
+
+    def verify_at_point(self, x=[[9], [-9]], eps=0.5, verbose=False):
+        self.build_problem_for_point(x=x, verbose=verbose)
+        try:
+            eps_hat = self.robustness_at_point(x,verbose=verbose)
+        except Exception as err:
+            print(f"ERROR: {err}")
+
+        if eps_hat < eps:
+            return False
+        return True
+
 
 
 if __name__ == '__main__':
+    # weights = [
+        # [[1, 0, 0, 0],
+         # [0, 1, 0, 0],
+         # [0, 0, 1, 0]],
+        # [[2, 0, 0],
+         # [0, 2, 0]],
+        # # [[3, 0],
+         # # [0, 3]]
+    # ]
+    # bias_vecs =[
+        # [1,1,1],
+        # [2,2],
+        # # [3,3],
+    # ]
+
+    # weights = [
+        # [[1, 0],
+         # [0, 1]],
+        # [[2, 0],
+         # [0, 2]],
+        # # [[3, 0],
+         # # [0, 3]]
+    # ]
+    # bias_vecs =[
+        # [1,1],
+        # [2,2],
+        # # [3,3],
+    # ]
 
     weights = [
         [[1, 0],
          [0, 1]],
-        [[2, 0],
-         [0, 2]],
-        # [[3, 0],
-         # [0, 3]]
+        [[1, 0],
+         [0, 1]],
     ]
     bias_vecs =[
-        (1,1),
-        (2,2),
-        # (3,3),
+        [0,0],
+        [0,0],
     ]
     f = MultiLayerNN(weights, bias_vecs)
     ilp = IteratedLinearVerifier(f)
-    ilp.verify_at_point()
-    # print(ilp)
-    # iterated_linear_program(x=[[9],[1]], eps=0.007, f=f)
+    eps = 0.5
+    x = [[0], [1]]
+    is_robust = ilp.verify_at_point(x=x, eps=eps)
+
+    # TODO: need some tolerance built in?
+    if is_robust:
+        print(f"Best z0: {ilp.opt_var.value}")
+        print(f"Identity map is ({eps})-robust at x={x}. epsilon-hat: {ilp.prob.value}")
+    else:
+        # print(ilp.str_constraints())
+        print(f"Best z0: {ilp.opt_var.value}")
+        print(f"\nERROR: Identity map is NOT ({eps})-robust at x={x}: epsilon_hat: {ilp.prob.value}")
+    # assert is_robust
