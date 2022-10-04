96fa37f4f95a57a8276e61478d04cbef8471b93b
Michael Roark <roarkmichael@gmail.com>
2022-10-04 11:17:00 -0700

    Fixup ILP tests

--- src/algorithms/test_ilp.py
+++ src/algorithms/test_ilp.py
@@ -6,23 +6,8 @@ from src.models.multi_layer import MultiLayerNN
 
 class TestIteratedLinearVerifier(unittest.TestCase):
 
-    # def test_constraints_for_seperating_hyperplane(self):
-        # weights = [
-            # [[1, 0],
-             # [0, 1]],
-            # [[1, 0],
-             # [0, 1]],
-        # ]
-        # bias_vecs =[
-            # (0,0),
-            # (0,0),
-        # ]
-        # f = MultiLayerNN(weights, bias_vecs)
-        # x = torch.rand((1,len(weights[0][0])))
-        # assert torch.equal(x, f(x))
-
-
     def test_identity_network(self):
+        # TODO: test combinations of parameterized dimension, depth
         weights = [
             [[1, 0, 0],
              [0, 1, 0],
@@ -53,8 +38,8 @@ class TestIteratedLinearVerifier(unittest.TestCase):
             # SCS hangs for eps >= 4
             ilp = IteratedLinearVerifier(f)
             epsilon = n + 0.3
-            self.assertTrue(ilp.verify_at_point(x=x, eps=epsilon),
-                            f"Identity map should be {epsilon}-robust at {x}.")
+            r = ilp.verify_at_point(x=x, eps=epsilon)
+            self.assertTrue(r, debug_failure("\nIdentity map SHOULD be robust", x, epsilon, ilp))
             del ilp
 
         # TODO: Bug! if we call this code block before the above, said test fails!
@@ -63,45 +48,15 @@ class TestIteratedLinearVerifier(unittest.TestCase):
             ilp = IteratedLinearVerifier(f)
             epsilon = 2**(-n)
             r = ilp.verify_at_point(x=x, eps=epsilon)
-            if r:
-                err = f"Identity map should not be robust at {x}.\n"
-                err += f"Epsilon: {epsilon} < epsilon_hat: {ilp.prob.value}\n"
-                err += f"Opt Var: {ilp.opt_var.value}\n"
-                self.assertFalse(True, err)
+            self.assertFalse(r, debug_failure("\nIdentity map SHOULD NOT be robust", x, epsilon, ilp))
+            del ilp
 
 
-    # def test_two_layer_identity_network(self):
-        # weights = [
-            # [[1, 0],
-             # [0, 1]],
-            # [[1, 0],
-             # [0, 1]],
-        # ]
-        # bias_vecs =[
-            # (0,0),
-            # (0,0),
-        # ]
-        # f = MultiLayerNN(weights, bias_vecs)
-        # x = torch.rand((1,2))
-        # assert torch.equal(x, f(x))
-
-        # # this fails if called after the decision boundary test!
-        # x = [[9], [0]]
-        # for n in range(1, 3):
-            # # SCS hangs for eps >= 4
-            # ilp = IteratedLinearVerifier(f)
-            # epsilon = n + 0.3
-            # self.assertTrue(ilp.verify_at_point(x=x, eps=epsilon),
-                            # f"Identity f should be {epsilon}-robust at {x}.")
-            # del ilp
-
-        # # TODO: Bug! if we call this code block before the above, said test fails!
-        # x = [[1], [1]]
-        # for n in range(5):
-            # ilp = IteratedLinearVerifier(f)
-            # epsilon = 2**(-n)
-            # self.assertFalse(ilp.verify_at_point(x=x, eps=epsilon),
-                             # f"Identity f should never be {epsilon}-robust at {x} (on the decision boundary).")
+def debug_failure(err_str, x, epsilon, ilp):
+    err_str += f" at {x}.\n"
+    err_str += f"Epsilon: {epsilon} < epsilon_hat: {ilp.prob.value}\n"
+    err_str += f"Opt Var:\n{ilp.opt_var.value}\n"
+    return err_str
 
 
 if __name__ == '__main__':
