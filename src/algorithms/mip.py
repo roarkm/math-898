b4d6015436481f931c97cc4c2fd0710386fd3208
Michael Roark <roarkmichael@gmail.com>
2022-11-11 20:08:24 -0800

    MIP working

--- src/algorithms/mip.py
+++ src/algorithms/mip.py
@@ -10,6 +10,7 @@ from src.algorithms.abstract_verifier import (AbstractVerifier,
 
 # TODO: log the branch and bound nodes that the optimizer builds for the MIP
 
+
 class MIPVerifier(AbstractVerifier):
 
     def __init__(self, f=None, M=10**3):
@@ -18,12 +19,15 @@ class MIPVerifier(AbstractVerifier):
         # self.solver = cp.ECOS_BB # has bugs
         self.solver = cp.GLPK_MI
         self.M = M
-        logging.basicConfig(format='MIP-%(levelname)s:\n%(message)s', level=logging.DEBUG)
+        logging.basicConfig(format='MIP-%(levelname)s:\n%(message)s',
+                            level=logging.INFO)
         self.prob = None
 
     def relu_constraints(self, Wi, layer_id):
-        self.add_free_var(cp.Variable((Wi.shape[0],1), f"z{layer_id}")) # post-activation
-        self.add_free_var(cp.Variable((Wi.shape[0],1), f"d{layer_id}", integer=True))
+        self.add_free_var(cp.Variable((Wi.shape[0], 1),
+                                      f"z{layer_id}"))
+        self.add_free_var(cp.Variable((Wi.shape[0], 1),
+                                      f"d{layer_id}", integer=True))
 
         # d_ij in {0,1}
         self.add_constraint(self.free_vars(f"d{layer_id}") >= 0,
@@ -36,30 +40,35 @@ class MIPVerifier(AbstractVerifier):
                             alg_type='mip')
 
         # z_ij >= z_ij_hat
-        self.add_constraint(self.free_vars(f"z{layer_id}") >= self.free_vars(f"z{layer_id}_hat"),
-                            layer_id=layer_id,
+        c = (self.free_vars(f"z{layer_id}")
+             >= self.free_vars(f"z{layer_id}_hat"))
+        self.add_constraint(c, layer_id=layer_id,
                             constr_type='relu_1',
                             alg_type='mip')
 
-        # z_ij_hat < 0 implies d_ij == 0    iff    -z_ij <= M * (1 - d_ij)
-        _c = -1*self.free_vars(f"z{layer_id}_hat") <= self.M * (np.ones((Wi.shape[0], 1))-self.free_vars(f"d{layer_id}"))
-        self.add_constraint(_c,
-                            layer_id=layer_id,
+        # z_ij <= z_ij_hat + M(1 - d_ij)
+        c = (self.free_vars(f"z{layer_id}")
+             <= (self.free_vars(f"z{layer_id}_hat")
+                 + cp.multiply(self.M, (np.ones((Wi.shape[0], 1))
+                                        - self.free_vars(f"d{layer_id}")))))
+        self.add_constraint(c, layer_id=layer_id,
                             constr_type='relu_2',
                             alg_type='mip')
 
-        # z_ij_hat > 0 implies d_ij == 1    iff    z_ij <= M * d_ij
-        _c = self.free_vars(f"z{layer_id}_hat") <= self.M * self.free_vars(f"d{layer_id}")
-        self.add_constraint(_c,
+        # z_ij_hat > 0 implies d_ij == 1 iff z_ij <= M * d_ij
+        c = (self.free_vars(f"z{layer_id}_hat")
+             <= cp.multiply(self.M, self.free_vars(f"d{layer_id}")))
+        self.add_constraint(c,
                             layer_id=layer_id,
                             constr_type='relu_3',
                             alg_type='mip')
 
     def network_constraints(self, _=None, verbose=False):
-        assert self.f != None, "No NN provided."
+        assert self.f is not None, "No NN provided."
 
         # optimization var list starting with z0
-        self.add_free_var(cp.Variable((self.nn_weights[0].shape[1],1), name='z0'))
+        self.add_free_var(cp.Variable((self.nn_weights[0].shape[1], 1),
+                                      name='z0'))
 
         for i in range(1, len(self.nn_weights)+1):
             # get layer weights and bias vec
@@ -76,34 +85,34 @@ class MIPVerifier(AbstractVerifier):
 
 
 def quick_test_eps_robustness():
-    f = identity_map(2,2)
+    f = identity_map(2, 2)
     mip = MIPVerifier(f)
     eps = 1
     x = [[9], [3]]
-    e_robust = mip.decide_eps_robustness(x, eps, verbose=True)
+    e_robust = mip.decide_eps_robustness(x, eps)
 
-    logging.debug(mip.str_constraints())
+    # print(mip.str_constraints())
 
     x_class = f.class_for_input(x)
     print(f"f({x}) = class {x_class+1}")
     print(f"{f.name} is ({eps})-robust at {x}?  {e_robust}")
     if not e_robust:
-        print(mip.str_opt_soln('z0'))
         ce = mip.counter_example
         print(f"Counterexample: f({ce}) = class {f.class_for_input(ce)+1}")
 
 
 def quick_test_pointwise_robustness():
-    f = identity_map(2,2)
+    f = identity_map(2, 2)
     mip = MIPVerifier(f)
     x = [[2.01], [1]]
     eps_hat = mip.compute_robustness(x)
 
-    logging.debug(mip.str_constraints())
+    # print(mip.str_constraints())
 
     print(f"Pointwise robusntess of {f.name} at {x} is {eps_hat}.")
     print(f"Nearest adversarial example is \n{mip.counter_example}.")
 
+
 if __name__ == '__main__':
     quick_test_eps_robustness()
-    quick_test_pointwise_robustness()
+    # quick_test_pointwise_robustness()
