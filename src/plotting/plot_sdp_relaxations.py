194d8c4c4fc6ea3f02b4bed2c13870fcaeb29057
Michael Roark <roarkmichael@gmail.com>
2022-11-27 13:55:52 -0800

    Plotting updates

--- src/plotting/plot_sdp_relaxations.py
+++ src/plotting/plot_sdp_relaxations.py
@@ -1,6 +1,11 @@
 from src.models.multi_layer import MultiLayerNN, identity_map
-from src.algorithms.certify import Certify, _relaxation_for_hypercube, _relaxation_for_half_space
+from src.algorithms.certify import (Certify,
+                                    _relaxation_for_hypercube,
+                                    _relaxation_for_half_space)
 from matplotlib.patches import Ellipse
+from src.plotting.common import *
+from src.plotting.plot_nn import (add_inf_ball,
+                                  add_relaxation)
 from src.algorithms.abstract_verifier import (constraints_for_separating_hyperplane,
                                               constraints_for_inf_ball)
 
@@ -23,13 +28,7 @@ def plot_half_space(c=[[-1], [1]], d=0, resolution=0.05,
     _x2 = np.arange(-1, 10, resolution)
     x1, x2  = np.meshgrid(_x1, _x2)
 
-    # test_point = np.array([[9], [0]])
-    # assert half_space_ind(9, 0) == 1
-    # assert half_space_ind(0, 9) == 0
-    # exit()
-
     z = half_space_ind(x1, x2)
-    # plt.grid(True)
     plt.axhline()
     plt.axvline()
     plt.contourf(x1, x2, z)
@@ -45,68 +44,31 @@ def plot_half_space(c=[[-1], [1]], d=0, resolution=0.05,
     plt.show()
 
 
-def plot_inf_ball(center, eps, values={'set':1, 'not_set':0},
-                  resolution=0.05, relaxation_matrix=None):
-    assert center.shape[0] == 2
+def plot_inf_ball(c, eps, values={'set':1, 'not_set':0},
+                  x_range=[-2,4], y_range=[-2,4],
+                  resolution=0.1, relaxation_matrix=None):
 
-    # free_vars = cp.Variable((len(center),1), name='z')
-    # constr, free_vars = constraints_for_inf_ball(center, eps, free_vars=free_vars)
-    constr, free_vars = constraints_for_inf_ball(center, eps)
-
-    ball_ind = make_set_indicator([constr], free_vars, values=values)
-
-    # TODO: parameterize the canvas size
-    _x1, _x2 = np.arange(-1, 3, resolution), np.arange(-1, 3, resolution)
+    _x1 = np.arange(x_range[0], x_range[1], resolution)
+    _x2 = np.arange(y_range[0], y_range[1], resolution)
     x1, x2  = np.meshgrid(_x1, _x2)
+    pad = 2
+    extent = np.min(x1)-pad, np.max(x1)+pad, np.min(x2)-pad, np.max(x2)+pad
 
-    z = ball_ind(x1, x2)
-    # plt.grid(True)
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+    plt.grid(True)
     plt.axhline()
     plt.axvline()
-    plt.contourf(x1, x2, z)
-    # plt.plot(center[0][0], center[1][0], marker="x", markersize=10)
-    plt.colorbar()
-    plt.xlabel('x1')
-    plt.ylabel('x2')
+
+    add_inf_ball(c[0][0], c[1][0], eps, ax)
 
     if relaxation_matrix is not None:
-        _qp = make_quadratic(relaxation_matrix)
-        z = _qp(x1, x2)
-        plt.contourf(x1, x2, z, alpha=0.2)
+        add_relaxation(relaxation_matrix, x1, x2, extent)
+
     plt.show()
-
-
-def make_set_indicator(constraints, free_vars, values={'set':1, 'not_set':0}):
-    # 2d only
-    expr = cp.transforms.indicator(constraints)
-    def indicator(x1, x2):
-        free_vars.value = np.array([[x1], [x2]])
-        if expr.value == 0:
-            return values['set'] # (x1, x2) satisfy the constraints
-        return values['not_set'] # (x1, x2) DO NOT satisfy the constraints
-
-    return np.vectorize(indicator)
-
-
-def make_half_space_indicator(c, d=0, values={'set':1, 'not_set':0}):
-    # 2d only
-    def indicator(x1, x2):
-        if (np.dot(c.T, np.array([[x1], [x2]])) > d):
-            return values['set'] # (x1, x2) satisfy the constraints
-        return values['not_set'] # (x1, x2) DO NOT satisfy the constraints
-    return np.vectorize(indicator)
-
-
-def make_quadratic(P):
-    # returns a function that computes the quadratic invoked by P
-    def M_quad(x1, x2):
-        _P = np.array(P)
-        _x = np.vstack(np.concatenate([[x1], [x2], np.array([1])]))
-        z =  _x.T @ _P @ _x
-        if z >= 0:
-            return 1
-        return 0
-    return np.vectorize(M_quad)
+    return
 
 
 def plot_relu(resolution=0.01):
@@ -139,20 +101,6 @@ def plot_relu(resolution=0.01):
     plt.show()
 
 
-def constraints_for_polytope():
-    # custom polytope
-    A = np.array([
-        [2,  1],
-        [-1, 0],
-        [0, -1]
-    ])
-    b = np.array([
-        [1, 2, 2]
-    ])
-    _x = cp.Variable((2, 1))
-    return [A @ _x <= b], _x
-
-
 def plot_constrained_region(constraints, free_vars,
                             values={'set':1, 'not_set':0},
                             colors={'bkgrnd':  (0.0, 0.0, 0.0, 0.0),
@@ -161,8 +109,6 @@ def plot_constrained_region(constraints, free_vars,
                             resolution=0.05,
                             plot_boundaries={'t':10, 'r':10, 'b':-10, 'l':-10},
                             plot_scale=1):
-    # plot the region satisfying constraints
-    # plot the image under relu of the region satisfying constraints
     _x1 = np.arange(plot_boundaries['l']*plot_scale,
                     plot_boundaries['r']*plot_scale,
                     resolution)
@@ -174,8 +120,6 @@ def plot_constrained_region(constraints, free_vars,
 
     fig, ax = plt.subplots()
     ax.axis('equal')
-    # plt.axhline(linestyle='--')
-    # plt.axvline(linestyle='--')
     plt.xlabel('x1')
     plt.ylabel('x2')
 
@@ -184,28 +128,25 @@ def plot_constrained_region(constraints, free_vars,
     cmap = ListedColormap([colors['not_set'], colors['set']])
     plt.imshow(z, interpolation='none',
                extent=extent, origin='lower', cmap=cmap)
-
-    # max_x1, max_x2 = max_in_region(constr, free_vars, x1, x2)
-    # relu_img = make_relu_img_indicator(constr, free_vars, max_x1, max_x2, values=values)
-    # img_z = relu_img(x1, x2)
-    # cmap = ListedColormap([colors['not_img'], colors['img']])
-    # plt.imshow(img_z, interpolation='none',
-               # extent=extent, origin='lower', cmap=cmap)
     plt.show()
 
 
 if __name__ == '__main__':
-    pt, fvars = constraints_for_polytope()
-    plot_constrained_region(pt, fvars)
+    # pt, fvars = custom_polytope_constraints()
+    # plot_constrained_region(pt, fvars)
+
     # plot_relu()
     # exit()
-    # center = np.array([[1], [1]])
-    # eps = 0.8
-    # # g_vals = np.array([9, 2])
+
+    center = np.array([[1], [1]])
+    eps = 0.8
+    g_vals = np.array([9, 2])
+    # np.random.seed(2)
+    # np.random.seed(179)
     # g_vals = 100 * np.random.random_sample(center.shape[0])
-    # P, _, _ = _relaxation_for_hypercube(center, eps, values=g_vals)
-    # # plot_inf_ball(center, eps, resolution=0.05, relaxation_matrix=P.value)
-    # plot_inf_ball(center, eps, resolution=0.08, relaxation_matrix=P.value)
+
+    P, _, _ = _relaxation_for_hypercube(center, eps, values=g_vals)
+    plot_inf_ball(center, eps, resolution=0.05, relaxation_matrix=P.value)
     # exit()
 
     # c = np.array([[-1], [1]])
